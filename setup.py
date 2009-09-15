8f6e5fe09c789c1c825eb3d5b2f76c9327746cca
Uli Fouquet <uli@gnufix.de>
2009-09-15 14:26:06 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='grokui.admin',
-      version='0.5dev',
+      version='0.6dev',
       description="The Grok administration and development UI (base)",
       long_description=(
           read(os.path.join('src', 'grokui', 'admin', 'README.txt')) +
