8b9de9f3f31c3fe98bf928025b42218aeada3e49
Uli Fouquet <uli@gnufix.de>
2010-04-06 12:02:37 +0000

    Comply with repository policy.

--- src/grokui/admin/messages.py
+++ src/grokui/admin/messages.py
@@ -1,5 +1,5 @@
 # -*- coding: latin-1 -*-
-# Copyright (c) 2007 gocept gmbh & co. kg
+# Copyright (c) 2007 Zope Foundation and Contributors
 # See also LICENSE.txt
 # $Id$
 """Support for flash-messages in the grok admin UI."""
