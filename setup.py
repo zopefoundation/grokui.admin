d45a3d9e4142111ef1e6eb5e23c022b7c3f12ab9
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2012-05-02 09:30:18 +0000

    Preparing release 0.11

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='grokui.admin',
-      version='0.11dev',
+      version='0.11',
       description="The Grok administration and development UI",
       long_description=(
           read(os.path.join('src', 'grokui', 'admin', 'README.txt')) +
@@ -44,8 +44,8 @@ setup(name='grokui.admin',
       namespace_packages=['grokui'],
       install_requires=[
           'ZODB3',
-          'grok >= 1.8',
-          'grokcore.site >= 1.5',
+          'grok >= 1.10',
+          'grokcore.site >= 1.6.1',
           'grokui.base',
           'setuptools',
           'z3c.flashmessage',
